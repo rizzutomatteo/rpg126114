89c6029effdff6790cf753a1ace7b9fe27575430
Matteo Rizzuto <matteorizzuto04@gmail.com>
2026-06-01 11:23:00 +0200

    Aggiunge la classe App provvisoria nel package di consegna
    Signed-off-by: Matteo Rizzuto <matteorizzuto04@gmail.com>
    
    Signed-off-by: Matteo Rizzuto <matteorizzuto04@gmail.com>

---
+++ app/src/main/java/it/unicam/cs/mpgc/rpg126114/App.java
@@ -0,0 +1,15 @@
+package it.unicam.cs.mpgc.rpg126114;
+
+/**
+ * Punto di ingresso provvisorio dell'applicazione.
+ * Verra' sostituito dall'interfaccia grafica JavaFX.
+ */
+public final class App {
+
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Pratiche Infernali - progetto in sviluppo");
+    }
+}
